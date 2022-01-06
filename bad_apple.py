b3f4e8ad19cbacadfc3502111ba2cb3d77b25775
YarnikeyZ <83335375+YarnikeyZ@users.noreply.github.com>
2022-01-06 13:46:34 +0500

    Add files via upload

---
+++ bad_apple.py
@@ -0,0 +1,32 @@
+from PIL import Image
+from time import sleep
+from datetime import datetime
+from subprocess import getoutput as gout
+clear = gout("clear")
+
+frames = 6573
+x = 200
+y = 100
+if x and y != None:
+	xtoy = x*y
+else:
+	xtoy = None
+sleeptime = 0.0199
+
+input('Press enter and watch...')
+
+starttime = datetime.now()
+for num in range(1, frames):
+	time = datetime.now()
+	file = f"/home/babushka/python_p/frames/image{num}.jpg"
+	img = Image.open(file)
+	if x and y != None:
+		img = img.resize((x, y), Image.ANTIALIAS)
+	zo = img.convert('L').point(lambda x: 0 if x < 128 else 1, '1')
+	data = list(zo.getdata())
+	start = str(data).replace('[', '').replace(']', '').replace(',', '').replace(' ', '')
+	center = '\n'.join([start[i:i+img.size[0]] for i in range(0, len(start), img.size[0])]) if ' ' not in start[:img.size[0]] and len(start) > img.size[0] else start
+	print(f'{sleep(sleeptime)}{clear}{str(center).replace("0"," ").replace("1","#")}\n\n"Screen": Resolution/Total/Sum: {x}x{y}/{xtoy}/{sum(data)} symbols\n\nFrames: Total/Remaining/Now frame(s): {frames}/{frames-num}/{num}\n\nTime: Total/Manual FT/Used FT: {str(datetime.now() - starttime)[3:]}/{sleeptime}/{str(datetime.now() - time)[6:][:5]}\n\a')
+
+##100x50 0.0199 ■
+##200x100 0.0125 #
